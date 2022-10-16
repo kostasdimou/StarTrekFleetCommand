5f89d6fdd28c7c816739cfe85642678917545699
Kostas Dimou <33748303+kostasdimou@users.noreply.github.com>
2022-10-16 17:49:56 +0300

    Create Building.tcl
    array BUILDING_UPGRADES stores the resources to upgrade a building to the level.

---
+++ Building.tcl
@@ -0,0 +1,15 @@
+array set BUILDING_UPGRADES {
+    {DEFENCE PLATFORM} {
+        29 {
+            {GAS UNCOMMON} 450
+            {GAS COMMON} 30
+            PARSTEEL 13.9M
+            TRITANIUM 410K
+            DILITHIUM 41K
+            UPGRADE {2h 9h}
+            INSTANT {
+                LATINUM 2396
+            }
+        }
+    }
+}
