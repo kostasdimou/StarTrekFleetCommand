0126ca5c50aa4e1b54109697f84a699eeb898e8c
Kostas Dimou <33748303+kostasdimou@users.noreply.github.com>
2022-10-16 21:35:29 +0300

    Update Ship.tcl

--- Ship.tcl
+++ Ship.tcl
@@ -1,20 +1,246 @@
 array set SHIP {
-    ENVOY {
-        TYPE Survey
-        x COMMON
-        BONUS {Tritanium Mining Laser}
+    1 {
+        CLASS Realta
+        TIER 4
+        LEVEL 20
+        XP MAX
+        STRENGTH 20833
+        ATTACK 574
+        DEFENSE 825
+        HEALTH 19K
+        {Enhanced Shield} 110%
+        {IMPULSE SPEED} 150
+        {WARP RANGE} 6
+        {WARP SPEED} 4.2
+        CARGO 4000
+        {PROTECTED CARGO} 468
+        {MINING BONUS} 100%
+    }
+    2 {
+        CLASS {Orion Corvette}
+        TIER 4
+        LEVEL 20
+        XP MAX
+        STRENGTH 25365
+        ATTACK 1482
+        DEFENSE 1630
+        HEALTH 22K
+        {Enhanced Shield} 110%
+        {IMPULSE SPEED} 115
+        {WARP RANGE} 8
+        {WARP SPEED} 4.2
+        CARGO 4000
+        {PROTECTED CARGO} 428
+        {MINING BONUS} 100%
+    }
+    3 {
+        CLASS {ECS Fortunate}
+        TIER 4
+        LEVEL 20
+        XP MAX
+        STRENGTH 30455
+        ATTACK 571
+        DEFENSE 2400
+        HEALTH 27K
+        {Enhanced Shield} 110%
+        {IMPULSE SPEED} 115
+        {WARP RANGE} 8
+        {WARP SPEED} 4.2
+        CARGO 4000
+        {PROTECTED CARGO} 428
+        {MINING BONUS} 100%
+    }
+    8 {
+        CLASS Envoy
         TIER 1
-        XP 0 
         LEVEL 1
+        XP 0 
         STRENGTH 31979
         ATTACK 2418
         DEFENSE 2235
         HEALTH 27K
-        {LEVEL UP} 130
+        CARGO x
+        {PROTECTED CARGO} x
+    }
+    - ------------------------
+    Realta {
+        RARITY Common
+        GRADE 1
+        SHIPYARD 1
+        TYPE Explorer
         MAX {
-            XP 130
-            LEVEL x
-            TIER 2
+            XP x
+            TIER 4
+            LEVEL 20
+        }
+        STRENGTH {
+            CONSTRUCTION 12524
+            SCRAPYARD 12524
+        }
+        BLUEPRINTS 10
+        WEAPON Energy
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Enhanced Shield}
+        {ABILITY RANGE} {- 110}
+    }    
+    {Orion Corvette} {
+        RARITY Common
+        GRADE 1
+        SHIPYARD 5
+        TYPE Battleship
+        MAX {
+            XP x
+            TIER 4
+            LEVEL 20
+        }
+        STRENGTH {
+            CONSTRUCTION 12583
+            SCRAPYARD 12583
+        }
+        BLUEPRINTS 15
+        WEAPON Energy
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Enhanced Hull}
+        {ABILITY RANGE} {- 110}
+    }
+    {ECS Fortunate} {
+        RARITY Common
+        GRADE 1
+        SHIPYARD 7
+        TYPE Survey
+        MAX {
+            XP x
+            TIER 4
+            LEVEL 20
+        }
+        STRENGTH {
+            CONSTRUCTION 16410
+            SCRAPYARD 16345
+        }
+        BLUEPRINTS 10
+        WEAPON Energy
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Parsteel Mining Laser}
+        {ABILITY RANGE} {- 50%}
+    }
+    {Phindra} {
+        RARITY Common
+        GRADE 2
+        SHIPYARD 10
+        TYPE Interceptor
+        MAX {
+            XP x
+            TIER 7
+            LEVEL 35
+        }
+        STRENGTH {
+            CONSTRUCTION 15998
+            SCRAPYARD 15862
+        }
+        BLUEPRINTS 30
+        WEAPON {kinetic kinetic}
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Battleship Hunter}
+        {ABILITY RANGE} {- 100%}
+    }
+    {Turas} {
+        RARITY Common
+        GRADE 2
+        SHIPYARD 12
+        TYPE Explorer
+        MAX {
+            XP x
+            TIER 7
+            LEVEL 35
+        }
+        STRENGTH {
+            CONSTRUCTION 19043
+            SCRAPYARD 18794
+        }
+        BLUEPRINTS 35
+        WEAPON {Energy Energy}
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Interceptor Hunter}
+        {ABILITY RANGE} {- 50%}
+    }
+    {Jellyfish} {
+        RARITY Rare
+        GRADE 2
+        SHIPYARD 12
+        TYPE Explorer
+        MAX {
+            XP x
+            TIER 7
+            LEVEL 35
+        }
+        STRENGTH {
+            CONSTRUCTION 23845
+            SCRAPYARD 23509
+        }
+        BLUEPRINTS 75
+        WEAPON {Energy Energy}
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Pilot Recognition}
+        {ABILITY RANGE} {- 70%}
+    }
+    {Talla} {
+        RARITY Common
+        GRADE 2
+        SHIPYARD 14
+        TYPE Battleship
+        MAX {
+            XP x
+            TIER 7
+            LEVEL 35
+        }
+        STRENGTH {
+            CONSTRUCTION 20326
+            SCRAPYARD 19752
+        }
+        BLUEPRINTS 40
+        WEAPON {Energy Energy}
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Explorer Hunter}
+        {ABILITY RANGE} {- 50%}
+    }
+    Envoy {
+        RARITY Common
+        GRADE 2
+        SHIPYARD 16
+        TYPE Survey
+        MAX {
+            XP x
+            TIER 7
+            LEVEL 35
+        }
+        STRENGTH {
+            CONSTRUCTION 19133
+            SCRAPYARD 19057
+        }
+        BLUEPRINTS 30
+        WEAPON {Energy}
+        FACTION -
+        CLASS -
+        CODE -
+        ABILITY {Tritanium Mining Laser}
+        {ABILITY RANGE} {- 150%}
+        {LEVEL UP} {
+            2 130
+            3 -
         }
         3 {
             {TIER UP} {
@@ -55,6 +281,111 @@ array set SHIP {
                 TRITANIUM 3200
                 DILITHIUM 775
             }
+            3 {
+            {TIER UP} {
+                {MAX LEVEL} +5
+                {CARGO CAPACITY} +12500
+                {PROTECTED CARGO} +3450
+                TIER {3h 35m}
+                INSTANT 47
+            }
+            {Mining Laser MK3} {
+                {{CRYSTAL UNCOMMON} 2} 140
+                {{SURVEY PARTS} 2} 43
+                TRITANIUM 9750
+                DILITHIUM 1225
+            }
+            {Shield MK3} {
+                BONUS {
+                    Defense 15%
+                    Health 3%
+                }
+                {{SURVEY PARTS} 2} 14
+                TRITANIUM 13K
+            }
+            {Warp Engines MK3} {
+                {{SURVEY PARTS} 2} 7
+                DILITHIUM 300
+            }
+            {Cargo Bay MK3} {
+                {{SURVEY PARTS} 2} 29
+                TRITANIUM 26.2K
+                DILITHIUM 450
+            }
+            {Phaser Emitter ENERGY MK3} {
+                BONUS {
+                    Attack 35%
+                }
+                {{SURVEY PARTS} 2} 22
+                TRITANIUM 3200
+                DILITHIUM 775
+            }
         }
+    }
+    {USS Franklin} {
+        RARITY Uncommon
+        GRADE 3
+        SHIPYARD 17
+        TYPE Explorer
+        MAX {
+            XP x
+            TIER 9
+            LEVEL 45
+        }
+        STRENGTH {
+            CONSTRUCTION 32092
+            SCRAPYARD 31608
+        }
+        BLUEPRINTS 90
+        WEAPON {Energy Energy Energy}
+        FACTION Federation
+        CLASS -
+        CODE NX-326
+        ABILITY {Sabotage}
+        {ABILITY RANGE} {12600% 12600%}
+    }
+    {Botany Bay} {
+        RARITY Uncommon
+        GRADE 3
+        SHIPYARD 18
+        TYPE Survey
+        MAX {
+            XP x
+            TIER 9
+            LEVEL 45
+        }
+        STRENGTH {
+            CONSTRUCTION 24391
+            SCRAPYARD 24274
+        }
+        BLUEPRINTS 60
+        WEAPON {Energy}
+        FACTION Federation
+        CLASS -
+        CODE -
+        ABILITY {Data Mining}
+        {ABILITY RANGE} {- 36000%}
+    }
+    {Botany Bay} {
+        RARITY Uncommon
+        GRADE 3
+        SHIPYARD 18
+        TYPE Survey
+        MAX {
+            XP x
+            TIER 9
+            LEVEL 45
+        }
+        STRENGTH {
+            CONSTRUCTION 24391
+            SCRAPYARD 24274
+        }
+        BLUEPRINTS 60
+        WEAPON {Energy}
+        FACTION Federation
+        CLASS -
+        CODE -
+        ABILITY {Data Mining}
+        {ABILITY RANGE} {- 36000%}
     }
 }
