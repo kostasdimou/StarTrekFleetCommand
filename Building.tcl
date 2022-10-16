9fec71693abe11ea67837442db838ffafad0413d
Kostas Dimou <33748303+kostasdimou@users.noreply.github.com>
2022-10-16 21:35:23 +0300

    Update Building.tcl

--- Building.tcl
+++ Building.tcl
@@ -1,14 +1,32 @@
 array set BUILDING_UPGRADES {
-    {DEFENCE PLATFORM} {
-        29 {
-            {GAS UNCOMMON} 450
-            {GAS COMMON} 30
-            PARSTEEL 13.9M
-            TRITANIUM 410K
-            DILITHIUM 41K
-            UPGRADE {2h 9h}
-            INSTANT {
-                LATINUM 2396
+    EXTERNAL {
+        {DEFENCE PLATFORM} {
+                {GAS UNCOMMON} 450
+                {GAS COMMON} 30
+                PARSTEEL 13.9M
+                TRITANIUM 410K
+                DILITHIUM 41K
+                UPGRADE {2h 9m}
+                INSTANT 2396
+            }
+        }
+    }
+    INTERNAL {
+        {Armada Control Center} {
+            LEVEL 18
+            MAX 50
+            29 {
+                BONUS {
+                    {Armada Damage Bonus} 58%
+                    {Armada Size} 4
+                }
+                {GAS COMMON} 30
+                {ARMADA TACTICAL CORE} 12K
+                PARSTEEL 3.4M
+                TRITANIUM 2M
+                DILITHIUM 205K
+                UPGRADE {1h 0m}
+                INSTANT 1261    
             }
         }
     }
