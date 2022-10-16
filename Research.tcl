c6612ee7624b6eda85650509c3dbaa485c55f590
Kostas Dimou <33748303+kostasdimou@users.noreply.github.com>
2022-10-16 18:17:28 +0300

    Create Research.tcl
    array RESEARCH stores the available researches per area.

---
+++ Research.tcl
@@ -0,0 +1,53 @@
+array set RESEARCH {
+    {OUTLAW RESEARCH} {
+        {Augmented Warp} {
+            LEVEL 1
+            MAX 5
+        }
+        {Prime Augment Favor} {
+            LEVEL 0
+            MAX 1
+        }
+        {Augmented Impulse} {
+            LEVEL 1
+            MAX 5
+        }
+        {Stella Research} {
+            LEVEL MAX
+        }
+        {x1} {
+        }
+        {x2} {
+        }
+        {Stella Hull} {
+            LEVEL MAX
+        }
+        {Crystal Research Efficiency} {
+            LEVEL 0
+            MAX 10
+            DESCRIPTION {Crystal cost efficiency for research}
+            1 {
+                BONUS 5%
+                {CRYSTAL UNCOMMON} 620
+                {OUTLAW RESEARCH CREDITS} 200
+                PARSTEEL 688K
+                DILITHIUM 168.3K
+                START {1d 22h}
+                INSTANT {LATINUM 831}
+            }
+        }
+        {Hideout Defenses} {
+            LOCKED
+        }
+        {Augmented Credit Yield} {
+            LEVEL 0
+            MAX 5
+        }
+        {Crystal Building Efficiency} {
+            LOCKED
+        }
+        {Assimilated Credit Yield} {
+            LOCKED
+        }
+    }
+}
